3ca0e440a47be77aa4129ef0c0d40ab5be938f06
jrcable1 <jrcable1@gmail.com>
2024-09-22 02:07:21 -0400

    raviole: override first API level build property.
    * Anything above 32 causes hardware enforcement. This
      allows us to continue to pass CTS checks.
    
    Change-Id: I4113b7378cd1a6c8cc6a553d0672833bb2285cfd
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+DEVICE_PATH := device/google/raviole
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raviole
+
+BUILD_BROKEN_DUP_RULES := true
