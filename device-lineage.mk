4d9cbb009b30c30bc39bbf074579d17a3e74bb7e
Jrcable1 <jrcable1@gmail.com>
2025-01-26 09:42:15 -0500

    Add missing IWLAN packages
    Change-Id: I671e4f057eb3fbe94f63dc16bf697867278e3eb2

--- device-lineage.mk
+++ device-lineage.mk
@@ -18,6 +18,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# IWLAN
+PRODUCT_PACKAGES += \
+    Iwlan
+
 # PixelParts (HBM)
 include packages/apps/PixelParts/device.mk
 
